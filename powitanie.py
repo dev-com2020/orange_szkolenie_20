58df0fa0a410b8f5b85b544c26ac73c8487c1ad8
devcom2020 <kwp@tlen.pl>
2022-10-20 12:46:51 +0200

    czwartek 12:46

---
+++ powitanie.py
@@ -0,0 +1,12 @@
+# To jest mój pierwszy program
+
+print("Python")
+print('Python')
+# print('👽👽👽')
+
+print('''
+    *************
+    *  👽👽👽  *    
+    *************
+    ''')
+input("Aby zakończyć wciśnij ENTER!")  # to jest instrukcja, która wprowadza dane z klawiatury
