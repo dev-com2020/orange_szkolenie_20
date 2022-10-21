787a98049f6bc82936189bd8c5fabb483e32155e
devcom2020 <kwp@tlen.pl>
2022-10-21 13:31:08 +0200

    piatek 13:32

--- dzien2/ify.py
+++ dzien2/ify.py
@@ -1,24 +1,23 @@
-lista = []
-name = input("Jak masz na imię")
-age = int(input("Ile masz lat"))
-
-print(f"Witaj {name}")
-if age < 18:
-    print("Nie masz jeszcze 18 lat")
-elif age == 18:
-    print("Masz dokładnie 18 lat")
-elif age == 40:
-    print("40-latek")
-else:
-    print(f"masz {age} lat")
-    lista.append(age)
-
-lista.append(name)
-print(f"Poza instrukcją, Zawartość listy: {lista}")
-
-# temp = int(input("Podaj temperaturę "))
-# minuta = int(input("Podaj minutę "))
+# lista = []
+# name = input("Jak masz na imię")
+# age = int(input("Ile masz lat"))
 #
+# print(f"Witaj {name}")
+# if age < 18:
+#     print("Nie masz jeszcze 18 lat")
+# elif age == 18:
+#     print("Masz dokładnie 18 lat")
+# elif age == 40:
+#     print("40-latek")
+# else:
+#     print(f"masz {age} lat")
+#     lista.append(age)
+#
+# lista.append(name)
+# print(f"Poza instrukcją, Zawartość listy: {lista}")
+
+
+
 # if temp < 20 and minuta > 10:
 #     print("działa w and!")
 #
@@ -27,3 +26,11 @@ print(f"Poza instrukcją, Zawartość listy: {lista}")
 #
 # if not (temp > 100):
 #     print("działa w not")
+
+temp = int(input("Podaj temperaturę "))
+if temp < 20:
+    minuta = int(input("Podaj minutę "))
+    if minuta > 10:
+        print("Parametry są właściwe")
+
+
