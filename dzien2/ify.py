7de98e5de06e836b69b3dc442aabb25fb669c1d3
devcom2020 <kwp@tlen.pl>
2022-10-21 11:08:24 +0200

    piatek 11:08

--- dzien2/ify.py
+++ dzien2/ify.py
@@ -1,24 +1,28 @@
+lista = []
+name = input("Jak masz na imię")
+age = int(input("Ile masz lat"))
 
-# name = input("Jak masz na imię")
-# age = int(input("Ile masz lat"))
+print(f"Witaj {name}")
+if age < 18:
+    print("Nie masz jeszcze 18 lat")
+elif age == 18:
+    print("Masz dokładnie 18 lat")
+elif age == 40:
+    print("40-latek")
+else:
+    print(f"masz {age} lat")
+    lista.append(age)
 
-# print(f"Witaj {name}")
-# if age < 18:
-#     print("Nie masz jeszcze 18 lat")
-# elif age == 18:
-#     print("Masz dokładnie 18 lat")
-# elif age > 18:
-#     print("Masz więcej niż 18 lat")
+print(f"Zawartość listy: {lista}")
 
-
-temp = int(input("Podaj temperaturę "))
-minuta = int(input("Podaj minutę "))
-
-if temp < 20 and minuta > 10:
-    print("działa w and!")
-
-if temp < 20 or minuta > 10:
-    print("działa w or!")
-
-if not (temp > 100):
-    print("działa w not")
+# temp = int(input("Podaj temperaturę "))
+# minuta = int(input("Podaj minutę "))
+#
+# if temp < 20 and minuta > 10:
+#     print("działa w and!")
+#
+# if temp < 20 or minuta > 10:
+#     print("działa w or!")
+#
+# if not (temp > 100):
+#     print("działa w not")
