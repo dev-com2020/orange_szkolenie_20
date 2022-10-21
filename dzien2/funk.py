78c55c2fc6d379a9a62ec6e48719480fc48b1477
devcom2020 <kwp@tlen.pl>
2022-10-21 14:47:15 +0200

    piatek 14:47

--- dzien2/funk.py
+++ dzien2/funk.py
@@ -4,6 +4,7 @@ a = random.randint(1, 100)
 b = random.randint(1, 100)
 x = int(input("Podaj liczbę x: "))
 
+
 def dodaj():
     global b
     a = int(input("Podaj a "))
@@ -16,8 +17,7 @@ def odejmij(a, b):
     print("Wynik odejmowania =", a - b)
 
 
-
-def pomnoz(a,x=1):
+def pomnoz(a, x=1):
     # x = 1000
     print(f"Liczba a={a}, liczba x={x}")
     print("Wynik mnożeniea =", a * x)
@@ -27,25 +27,39 @@ def podziel():
     pass
 
 
+def suma(*args):
+    lista = []
+    while True:
+        wejscie = input()
+        if wejscie == "stop":
+            break
+        lista.append(int(wejscie))
+    print("Suma podanych parametrów to:", sum(lista))
+
+
 while True:
     print('''
        *** MENU ***
         1. dodaj
         2. odejmij
         3. pomnóż
-        4. podziel 
+        4. podziel(chwilowo nieczynne) 
         5. koniec
+        t = test sumy
         ''')
     w = input("Mój wybór: ")
     if w == "1":
         dodaj()
     elif w == "2":
-        odejmij(a,b)
+        odejmij(a, b)
     elif w == "3":
-        pomnoz(a,x)
+        pomnoz(a, x)
     elif w == "4":
         podziel()
     elif w == "5":
         break
+    elif w == "t":
+        suma()
+
     else:
         print("Nie ma takiej opcji, spróbuj ponownie")
